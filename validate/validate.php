ab0ac941c2d9f0ac20a7ee93dbbc5147e3122c4d
Zhiqiao Xu <xuzhiqiao97@gmail.com>
2016-08-24 21:37:59 +0800

    Update validate.php

--- validate/validate.php
+++ validate/validate.php
@@ -10,11 +10,5 @@ function isMobile($mobile) {
     if (empty($mobile)) {
         return false;
     }
-
-    if($mobile == '4000020188') return true;
-    //return preg_match('#^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,6,7,8]{1}\d{8}$|^18[\d]{9}$#', $mobile) ? true : false;
-    if(is_numeric($mobile) && strlen($mobile) == 11) {
-        return true;
-    }
-    return false;
+    return preg_match('#^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,6,7,8]{1}\d{8}$|^18[\d]{9}$#', $mobile) ? true : false;
 }
