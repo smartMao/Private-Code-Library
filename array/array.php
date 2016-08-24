cb54c64b47d458b35ba8e7d8f4d2230b681b3c4b
Zhiqiao Xu <xuzhiqiao97@gmail.com>
2016-08-24 21:33:58 +0800

    Update array.php

--- array/array.php
+++ array/array.php
@@ -1,1 +1,25 @@
 
+
+/**
+ * 根据二维数组中的某个键进行排序
+ * @param $array
+ * @param $key
+ * @param string $order
+ * @return array
+ */
+function arr_sort($array, $key, $order="DESC" ){
+    $arr_nums = $arr = array();
+    foreach($array as $k=>$v){
+        $arr_nums[$k] = $v[$key];
+    }
+    if($order == 'asc'){
+        asort($arr_nums);
+    }else{
+        arsort($arr_nums);
+    }
+    foreach($arr_nums as $k=>$v){
+        $arr[$k] = $array[$k];
+    }
+    $arr = array_values($arr);
+    return $arr;
+}
