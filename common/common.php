a5fd58fb67ed877caf30758f481a32489a4e4be0
Zhiqiao Xu <xuzhiqiao97@gmail.com>
2016-08-24 21:33:33 +0800

    Update common.php

--- common/common.php
+++ common/common.php
@@ -1,1 +1,12 @@
 
+/**
+ * 返回当前的毫秒数
+ */
+function countExecTime()
+{
+    $time = explode ( " ", microtime () );
+    $time = $time [1] . ($time [0] * 1000);
+    $time2 = explode ( ".", $time );
+    $time = $time2 [0];
+    return $time;
+}
